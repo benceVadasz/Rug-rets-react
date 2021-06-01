9be4d01c6c6e44bc3a3e0cddc99167989459d52d
vadaszbence <64799151+vadaszbence@users.noreply.github.com>
2021-06-01 16:44:56 +0200

    add box shadow to switcher

--- src/components/DesignTypeSwitcher.js
+++ src/components/DesignTypeSwitcher.js
@@ -8,6 +8,7 @@ const DesignTypeSwitcher = () => {
     const useStyles = makeStyles(() => ({
         switcher: {
             height: '50%',
+            boxShadow: 'rgba(0, 0, 0, 0.12) 0px 1px 3px, rgba(0, 0, 0, 0.24) 0px 1px 2px',
         }
     }));
     const classes = useStyles();
