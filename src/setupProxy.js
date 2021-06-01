98c94ca42a5307b9f1bd8a7131e0f167504cd28f
vadaszbence <64799151+vadaszbence@users.noreply.github.com>
2021-06-01 16:44:28 +0200

    add proxy setup

---
+++ src/setupProxy.js
@@ -0,0 +1,12 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function(app) {
+    app.use(
+        '/Rug-Rets',
+        createProxyMiddleware({
+            target: 'http://localhost',
+            changeOrigin: true,
+        })
+    );
+};
+
