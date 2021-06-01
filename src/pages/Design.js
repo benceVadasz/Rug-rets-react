a103a7407dd3342a5bdd1578bf89bb970373432f
vadaszbence <64799151+vadaszbence@users.noreply.github.com>
2021-06-01 07:55:37 +0200

    create grid for design page

--- src/pages/Design.js
+++ src/pages/Design.js
@@ -3,10 +3,35 @@ import {makeStyles} from '@material-ui/core/styles';
 
 
 const Design = () => {
-    const useStyles = makeStyles((theme) => ({
+    const useStyles = makeStyles(() => ({
+
+        designContainer: {
+            height: '90vh',
+            width: '100%',
+            display: 'grid',
+            gridTemplateColumns: 'repeat(5, 1fr)',
+            gridTemplateRows: 'repeat(8, 1fr)',
+            gridColumnGap: 0,
+            gridRowGap: 0,
+        },
+        designTypesContainer: {
+            gridArea: '1 / 1 / 2 / 6',
+        },
+        shapePicker: {
+            gridArea: '2 / 1 / 4 / 6'
+        },
+        canvas: {
+            gridArea: '4 / 1 / 9 / 5'
+        },
+        colorPicker: {
+            gridArea: '4 / 5 / 9 / 6'
+        },
+        buttonContainer: {
+            gridArea: '8 / 5 / 9 / 6'
+        }
 
     }));
-
+    const classes = useStyles();
     // if (loading)
     //     return (
     //         <div className={classes.load}>
@@ -15,7 +40,22 @@ const Design = () => {
     //     );
 
     return (
-        <div>
+        <div className={classes.designContainer}>
+            <div className={classes.designTypesContainer}>
+
+            </div>
+            <div className={classes.shapePicker}>
+
+            </div>
+            <div className={classes.canvas}>
+
+            </div>
+            <div className={classes.colorPicker}>
+
+            </div>
+            <div className={classes.buttonContainer}>
+
+            </div>
         </div>
     );
 }
