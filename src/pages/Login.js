033ae33d36ca2698be76b3d8675f5625830b47d8
vadaszbence <64799151+vadaszbence@users.noreply.github.com>
2021-06-02 18:40:30 +0200

    add google authentication for login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,6 @@
 import React, {useState} from "react";
 import axios from 'axios';
+import {GoogleLogin} from "react-google-login";
 import {makeStyles} from '@material-ui/core/styles';
 import {
     Grid,
@@ -10,10 +11,14 @@ import {
     Button,
 } from "@material-ui/core";
 import AddCircleOutlineOutlinedIcon from "@material-ui/icons/AddCircleOutlineOutlined";
-import { BASE_URL } from "../constants";
+import {BASE_URL} from "../constants";
+import Icon from '../assets/icon';
+import { useDispatch } from "react-redux";
+import { useHistory } from 'react-router-dom';
 // import Loading from "./Loading";
 
 const Login = () => {
+    const history = useHistory();
     const useStyles = makeStyles(() => ({
         paperStyle: {
             padding: "30px 20px",
@@ -39,12 +44,12 @@ const Login = () => {
             flexFlow: "column wrap",
             justifyContent: "space-between",
         },
-        headerStyle: { margin: 0, fontFamily: "'IBM Plex Mono', monospace" },
-        avatarStyle: { backgroundColor: "#edede9",  },
-        button: { backgroundColor: "#edede9" },
-        passwordStyle: { marginBottom: 30 },
-        invalid: { color: 'red' },
-        load: { position: "fixed", top: "50%", left: "50%", transform: "translate(-50%, -50%)" }
+        headerStyle: {margin: 0, fontFamily: "'IBM Plex Mono', monospace"},
+        avatarStyle: {backgroundColor: "#edede9",},
+        button: {backgroundColor: "#edede9", marginBottom: 20},
+        passwordStyle: {marginBottom: 30},
+        invalid: {color: 'red'},
+        load: {position: "fixed", top: "50%", left: "50%", transform: "translate(-50%, -50%)"}
     }));
 
     const classes = useStyles();
@@ -52,6 +57,7 @@ const Login = () => {
     const [password, setPassword] = useState("");
     const [loading, setLoading] = useState(false);
     const [invalidCredentials, setInvalidCredentials] = useState(false);
+    const dispatch = useDispatch();
 
 
     const setEmail = (e) => {
@@ -84,6 +90,22 @@ const Login = () => {
             });
     };
 
+    const googleSuccess = async (res) => {
+        const result = res?.profileObj;
+        const token = res?.tokenId;
+        try {
+            dispatch({ type: 'AUTH', data: { result, token } })
+            history.push('/')
+        }
+        catch (e) {
+            console.log(e)
+        }
+    };
+
+    const googleFailure = () => {
+        console.log('google sign in was unsuccessful. Please refresh and start again.')
+    };
+
 
     // if (loading)
     //     return (
@@ -98,14 +120,15 @@ const Login = () => {
                 <Paper elevation={20} className={classes.paperStyle}>
                     <Grid align="center">
                         <Avatar className={classes.avatarStyle}>
-                            <AddCircleOutlineOutlinedIcon />
+                            <AddCircleOutlineOutlinedIcon/>
                         </Avatar>
                         <h2 className={classes.headerStyle}>Log in</h2>
-                        {!invalidCredentials ? <Typography className={classes.headerStyle} variant="caption" gutterBottom>
-                            Please fill this form to log in!
-                        </Typography> : <Typography className={classes.invalid} variant="caption" gutterBottom>
-                            Invalid credentials
-                        </Typography>}
+                        {!invalidCredentials ?
+                            <Typography className={classes.headerStyle} variant="caption" gutterBottom>
+                                Please fill this form to log in!
+                            </Typography> : <Typography className={classes.invalid} variant="caption" gutterBottom>
+                                Invalid credentials
+                            </Typography>}
                     </Grid>
                     <form onSubmit={submit} className={classes.formStyle}>
                         <TextField
@@ -124,9 +147,25 @@ const Login = () => {
                             placeholder="Enter your password"
                             onChange={(e) => setPassword(e.target.value)}
                         />
-                        <Button type="submit" variant="contained" className={classes.button}>
+                        <Button type="submit" variant="contained" className={classes.button + ' lower-case'}>
                             Log in
                         </Button>
+                        <GoogleLogin
+                            clientId='525017423702-622ugttvcve20rljokq90t37rdl8m4bc.apps.googleusercontent.com'
+                            render={(renderProps) => (
+                                <Button className={'lower-case white ' + classes.googleBtn} color="primary"
+                                        fullWidth onClick={renderProps.onClick}
+                                        disabled={renderProps.disabled}
+                                        startIcon={<Icon/>}
+                                        variant="contained"
+                                >
+                                     Sign in with Google
+                                </Button>
+                            )}
+                            onSuccess={googleSuccess}
+                            onFailure={googleFailure}
+                            cookiePolicy="single_host_origin"
+                        />
                     </form>
                 </Paper>
             </Grid>
