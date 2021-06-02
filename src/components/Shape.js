fbab32ba921cd2978339376e5a09a887bce4f33e
vadaszbence <64799151+vadaszbence@users.noreply.github.com>
2021-06-02 18:42:19 +0200

    add basic skeleton for shape component

---
+++ src/components/Shape.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { useSelector } from "react-redux";
+import {makeStyles} from '@material-ui/core/styles';
+
+
+const Shape = () => {
+
+    const useStyles = makeStyles(() => ({
+
+    }));
+    const classes = useStyles();
+
+    return (
+        <div >
+
+        </div>
+    );
+}
+
+export default Shape;
