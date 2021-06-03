c0e3e617ea7852706890c1eac0669021cb84335c
vadaszbence <64799151+vadaszbence@users.noreply.github.com>
2021-06-03 19:55:21 +0200

    style up shape picker container and include shape adder component

--- src/components/ShapePicker.js
+++ src/components/ShapePicker.js
@@ -2,20 +2,25 @@ import React from 'react';
 import { useSelector } from "react-redux";
 import {makeStyles} from '@material-ui/core/styles';
 import Shape from "./Shape";
+import ShapeAdder from "./ShapeAdder";
 
 
 const ShapePicker = () => {
 
     const shapes = useSelector((state => state.shapes))
-    console.log(shapes)
-    const useStyles = makeStyles(() => ({
 
+    const useStyles = makeStyles(() => ({
+        colorSelector: {
+            display: 'flex',
+            alignItems: 'flex-start',
+        }
     }));
     const classes = useStyles();
+    // todo: check what the design type selection is in redux store | fetch it in useEffect
 
     return (
         <div className={classes.colorSelector}>
-
+            <ShapeAdder />
         </div>
     );
 }
