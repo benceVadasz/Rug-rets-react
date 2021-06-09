927ae4f95bcba93e55ccf98402447df89838afbf
vadaszbence <64799151+vadaszbence@users.noreply.github.com>
2021-06-09 19:15:27 +0200

    implement dynamic import method

--- src/components/Rug.js
+++ src/components/Rug.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 
+const Rug = ({file, onFill, fillColors}) => {
+    const ImportedIconRef = useRef(null);
+    const [loading, setLoading] = useState(false);
+    useEffect(() => {
+        if (file.length > 0) {
+            setLoading(true);
+            const importIcon = async () => {
+                try {
+                    const {default: namedImport} = await import(`../assets/rugShapes/${file}.js`);
+                    ImportedIconRef.current = namedImport;
+                } catch (err) {
+                    throw err;
+                } finally {
+                    setLoading(false);
+                }
+            };
+            importIcon();
+        }
+    }, [file]);
 
-const Rug = ({onFill, fillColors, file}) => {
-    const {Img} = import("../assets/rugShapes/" + file + 'js')
-    return (
-        <Img/>
-    );
+    if (!loading && ImportedIconRef.current) {
+        const {current: ImportedIcon} = ImportedIconRef;
+        return <ImportedIcon onFill={onFill} fillColors={fillColors}/>;
+    }
+
+    return null;
+
 }
-
 
 export default Rug;
