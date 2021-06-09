5fbbbda2f518b4259495c013c70962ce0d8a15ab
vadaszbence <64799151+vadaszbence@users.noreply.github.com>
2021-06-09 19:17:20 +0200

    remove import

--- src/pages/Design.js
+++ src/pages/Design.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React from "react";
 import {makeStyles} from '@material-ui/core/styles';
 import ColorPicker from "../components/ColorPicker";
 import DesignTypeSwitcher from "../components/DesignTypeSwitcher";
